a18dbdb41c7dccfc38145968ede0754fbc31bacd
mase-git <kode.gcloud@gmail.com>
2023-01-24 23:15:00 +0100

    Cleaning spotipy dependencies and refactor entire package

--- spotrend/exceptions.py
+++ spotrend/exceptions.py
@@ -1,8 +1,55 @@
+class SpotifyAPIAuthError(Exception):
+    """
+    This exception would be raised when there is an issue 
+    with authenticating with the Spotify API, 
+    such as an invalid access token or client ID.
+    """
+    pass
 
-class SpotrendsInputException(Exception):
 
-    def __init__(self, msg):
-        self.msg = msg
+class SpotifyAPIRequestError(Exception):
+    """
+    This exception would be raised when there is an issue 
+    with making a request to the Spotify API, such as a network 
+    error or invalid endpoint.
+    """
+    pass
 
-    def __str__(self):
-        return self.msg
+
+class SpotifyAPIPermissionError(Exception):
+    """
+    This exception would be raised when the user does not 
+    have sufficient permissions to perform the requested action, 
+    such as trying to modify a playlist they do not own.
+    """
+    pass
+
+
+class SpotifyAPINotFoundError(Exception):
+    """
+    This exception would be raised when the requested 
+    resource is not found, such as a track or album that does not exist.
+    """
+    pass
+
+
+class SpotifyAPIInvalidDataError(Exception):
+    """
+    This exception would be raised when the data passed 
+    to the API is invalid, such as an invalid track URI or playlist name.
+    """
+    pass
+
+
+class SpotifyAPIQuotaError(Exception):
+    """
+    This exception would be raised when the user has reached their rate limit for the day.
+    """
+    pass
+
+
+class SpotifyAPIServerError(Exception):
+    """
+    This exception would be raised when there is an issue on Spotify server side.
+    """
+    pass
