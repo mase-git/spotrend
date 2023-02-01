efa5207a09bed88a3e2ce6988a2ed6586b3038ce
mase-git <kode.gcloud@gmail.com>
2023-02-01 21:18:46 +0100

    Remove old tests

--- tests/unit/client_test.py
+++ tests/unit/client_test.py
@@ -7,21 +7,6 @@ class ClientTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
     
-    def test_get_credentials(self):
-        # remove temporal key
-        self.client.id = None
-        with self.assertRaises(SpotrendAuthError) as context:
-            self.client.get_client_credentials()
-        
-        self.assertEqual("Invalid user client credentials", str(context.exception))
-        # reset the client for other tests
-        self.client = Client()
-    
-    def test_access_token(self):
-        # when the client is started, we don't have token set,
-        # we can easily get it from Spotify API automatically inside the access_token() method
-        self.client = Client() # no token at this state
-        token = self.client.get_access_token()
-        # the client obtains and set directly the new token
-        self.assertEqual(self.client.access_token, token)
+
+
 
